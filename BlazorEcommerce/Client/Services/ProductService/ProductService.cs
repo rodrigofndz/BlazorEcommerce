7f87fae1e212da64520418d92e8b4b0c547f4481
rodrigo <fernandezsanchezr19@gmail.com>
2023-03-01 13:42:49 +0100

    Added a Product Service Layer in Server and Client side.

---
+++ BlazorEcommerce/Client/Services/ProductService/ProductService.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorEcommerce.Client.Services.ProductService
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _http;
+
+        public ProductService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public async Task GetProductsAsync()
+        {
+            var result = 
+                await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product");
+            Products = result.Data;
+        }
+    }
+}
