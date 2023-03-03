0be7cea189ff640676cdcebad4f52ddee6b0d784
rodrigo <fernandezsanchezr19@gmail.com>
2023-03-03 11:16:05 +0100

    Added a CategoryService in the client side

---
+++ BlazorEcommerce/Client/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,22 @@
+﻿using BlazorEcommerce.Shared;
+
+namespace BlazorEcommerce.Client.Services.CategoryService
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly HttpClient _http;
+
+        public CategoryService(HttpClient http)
+        {
+            _http = http;
+        }
+        public List<Category> Categories { get; set; } = new List<Category>();
+
+        public async Task GetCategoriesAsync()
+        {
+            var result =
+                await _http.GetFromJsonAsync<ServiceResponse<List<Category>>>("api/categories");
+            Categories = result.Data;
+        }
+    }
+}
